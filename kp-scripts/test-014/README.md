cf3ea0cf9f373503b0d0031e6baa7cbad9ef0885
Konstantin Pandl <konstantinpandl@yahoo.de>
2023-12-04 23:37:53 +0100

    local devnet

--- kp-scripts/test-014/README.md
+++ kp-scripts/test-014/README.md
@@ -1,2 +1,4 @@
 20 node setup, nodes 4-19 bond/unbond
 Unlike in test-013, the stake distribution is realatively even across all nodes
+
+Same as test-014, but in a local devnet
---
+++ kp-scripts/test-015/01_perma_start_4_nodes.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Read the total number of validators from the user or use a default value of 4
+read -p "Enter the total number of validators (default: 4): " total_validators
+total_validators=${total_validators:-4}
+
+# Create a timestamp-based directory for log files
+log_dir=".logs"
+mkdir -p "$log_dir"
+
+# Create a new tmux session named "devnet"
+tmux new-session -d -s "devnet" -n "window0"
+
+# Generate validator indices from 0 to (total_validators - 1)
+validator_indices=($(seq 0 $((total_validators - 1))))
+
+# Loop through the list of validator indices and create a new window for each
+for validator_index in "${validator_indices[@]}"; do
+  # Generate a unique and incrementing log file name based on the validator index
+  log_file="$log_dir/validator-$validator_index.log"
+
+  # Create a new window with a unique name
+  tmux new-window -t "devnet:$validator_index" -n "window$validator_index"
+
+  # Send the command to start the validator to the new window and capture output to the log file
+  tmux send-keys -t "devnet:window$validator_index" "snarkos start --nodisplay --dev $validator_index --dev-num-validators $total_validators --validator --verbosity 0 --logfile $log_file" C-m
+done
+
+# Attach to the tmux session to view and interact with the windows
+tmux attach-session -t "devnet"
